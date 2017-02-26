4a75be3b34c53f1eadf5c20e2f95600a2a064dba
apmihal <apmihal@gmail.com>
2017-02-26 07:10:43 -0600

    Add files via upload
    Shouts at user if they don't have the correct number of command line arguments.

---
+++ cleanup.py
@@ -0,0 +1,31 @@
+
+import os
+from sys import argv
+
+def cleanUp():
+    """Looks in startPath for all filenames containing argv[1], creates and
+    moves them to a sub-directory with the same name."""
+    if len(argv) != 2:
+        print("Use: specify the string you'd like to search for in filenames as a command line argument.")
+        return 1
+
+    startPath = #Path of directory you'd like to clean up.
+    targetPath = os.path.join(startPath, argv[1])
+    print(targetPath)
+
+    if not os.path.exists(targetPath):
+        os.makedirs(targetPath)
+
+    for filename in os.listdir(startPath):
+        #Change the extension to whatver type of file you'd like to clean up.
+        if argv[1] in filename and filename.endswith('.jpg'):
+            currentLoc = os.path.join(startPath, filename)
+            targetLoc = os.path.join(targetPath, filename)
+            os.rename(currentLoc, targetLoc)
+            print(filename)
+
+def main():
+    cleanUp()
+
+if __name__ == '__main__':
+    main()
